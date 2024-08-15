56fff95fd48773fa946155f4a14d3b4707f0e781
MinTiger <orum1223@gmail.com>
2024-08-15 15:30:47 +0900

    REFACTOR.일반 회원가입, 로그인 문제 해결 일반로그인, 회원가입 로직 auth파일로 옮겼습니다

--- config/swagger.config.js
+++ config/swagger.config.js
@@ -5,7 +5,8 @@ const options = {
     info: {
       title: "WASH API",
       version: "1.0.0",
-      description: "wash api",
+      description:
+        "wash api / 헤더로 토큰 넣으실때 {Bearer AcessToken} 형식으로 중괄호 내부 값만 넣으셔야 합니다",
     },
     host: "localhost:3000",
     basepath: "../",

7aee5fed4e5a47d66780e65fb7dc52cac177b286
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2024-08-31 00:41:52 +0900

    refactor : 구독 시 user 테이블 수정

--- srcs/subscription/subscription.model.js
+++ srcs/subscription/subscription.model.js
@@ -5,6 +5,8 @@ export const SubscriptionModel = {
   subscribe: async (userId) => {
     try {
       const [result] = await pool.query(sql.subscribe, [userId]);
+      // user 테이블의 is_subscribed 컬럼을 0, 아니면 1로 업데이트
+      await pool.query(sql.updateUserSubscribe, [0, userId]);
       return result;
     } catch (error) {
       throw error;
@@ -14,6 +16,9 @@ export const SubscriptionModel = {
   cancelSubscribe: async (userId) => {
     try {
       const [result] = await pool.query(sql.cancelSubscribe, [userId]);
+      console.log(sql.updateUserSubscribe);
+      console.log(userId);
+      await pool.query(sql.updateUserSubscribe, [1, userId]);
       return result;
     } catch (error) {
       throw error;
--- srcs/subscription/subscription.sql.js
+++ srcs/subscription/subscription.sql.js
@@ -3,6 +3,12 @@ export const sql = {
     INSERT INTO subscription (user_id, status, start_date, end_date, payment_status, amount, payment_method, payment_date, transaction_id)
       VALUES (?, '구독', NOW(), DATE_ADD(NOW(), INTERVAL 1 MONTH), '성공', 50000, '카드', NOW(), '123456789');
   `,
+
+  updateUserSubscribe: `
+    UPDATE user
+      SET subscribe = ?
+    WHERE user_id = ?;
+  `,
   cancelSubscribe: `
     UPDATE subscription
       SET status = '비구독', end_date = NOW()
